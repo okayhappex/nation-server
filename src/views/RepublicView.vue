04efebb91cabc98d39b7c5cc423ba63f96ec1c09
happex <110610727+okayhappex@users.noreply.github.com>
2024-08-14 21:50:27 +0200

    Update RepublicView.vue

--- src/views/RepublicView.vue
+++ src/views/RepublicView.vue
@@ -2,5 +2,5 @@
     document.title = "Nation - Gouvernement"
 </script>
 <template>
-    <h1 class="text-4xl font-bold text-center my-64">Serveur pas encore ouvert.</h1>
+    <h1 class="text-4xl font-bold text-center my-32">Serveur pas encore ouvert.</h1>
 </template>
