496b80842337d354ea4f8372318dc9bc41a90d99
happex <110610727+okayhappex@users.noreply.github.com>
2024-08-16 19:45:35 +0200

    Amélioration de la navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -3,12 +3,21 @@
 </script>
 <template>
     <nav class="flex bg-primary h-16 overflow-hidden dark:bg-zinc-800">
-        <RouterLink to="/" class="float-left px-5 place-content-center">
+        <RouterLink to="/" class="px-6 place-content-center">
             <img src="@/assets/logo_with_text.png" class="h-9" />
         </RouterLink>
+        <RouterLink to="/about" class="hidden text-white font-semibold h-full px-6 place-content-center sm:block">
+            Infos
+        </RouterLink>
+        <RouterLink to="/gov" class="hidden text-white font-semibold h-full px-6 place-content-center sm:block">
+            Gouvernement
+        </RouterLink>
         <div class="grow"></div>
-        <a href="https://dsc.gg/nationserver" class="float-left h-full px-5 place-content-center">
-            <button class="bg-indigo-400 text-sm font-medium rounded-md px-5 h-10">Rejoindre</button>
+        <a href="https://dsc.gg/nationserver" class="h-full px-4 place-content-center">
+            <img src="@/assets/social/discord-logo.png" class="w-6" />
+        </a>
+        <a href="https://github.com/1nserv" class="hidden h-full px-4 place-content-center sm:block">
+            <img src="@/assets/social/github-logo.png" class="w-6" />
         </a>
     </nav>
 </template>
